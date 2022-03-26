97896417302469f7b75c7ed8a110e794792be2fa
pythonlove-r <74693532+pythonlove-r@users.noreply.github.com>
2022-03-26 19:51:29 -0400

    Update octoprimetire.py

--- tire/octoprimetire.py
+++ tire/octoprimetire.py
@@ -1,1 +1,17 @@
+from tire.tire import Tire
+from utils import add_years_to_date
 
+
+class CarriganTire(Battery):
+    def __init__(self, num1, num2,num3,num4):
+        self.num1 = num1
+        self.num2 = num2
+        self.num3 = num3
+        self.num4 = num4
+
+
+    def needs_service(self):  
+        if num1<0.9&num2<0.9&num3<0.9&num4<0.9
+            return False
+        else:
+            return True
